38cd19cf1eea9b53c8ebce48f8a2f10ff186360e
AmrAngry <Amr.Elghadban@seddiqiholding.com>
2023-05-19 09:25:44 +0400

    remove pods files from example project

--- Example/Pods/Target Support Files/Pods-TagsCollection_Example/Pods-TagsCollection_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TagsCollection_Example : NSObject
-@end
-@implementation PodsDummy_Pods_TagsCollection_Example
-@end
--- Example/Pods/Target Support Files/TagsCollection/TagsCollection-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_TagsCollection : NSObject
-@end
-@implementation PodsDummy_TagsCollection
-@end
